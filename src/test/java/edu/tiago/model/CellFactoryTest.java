aa694768932bc273496baeeaeacd83664742269a
brtiago <tgribeiro@gmail.com>
2025-08-19 16:05:20 -0300

    feat: creating some celltests

--- src/test/java/edu/tiago/model/CellFactoryTest.java
+++ src/test/java/edu/tiago/model/CellFactoryTest.java
@@ -8,10 +8,44 @@ class CellFactoryTest {
 
     @Test
     void shouldCreateFixedCell() {
-        Cell cell = Cell.createCell(0, 0, 5, true);
+        // Arrange
+        int row = 0, col = 0, value = 5;
+        boolean isFixed = true;
+
+        // Act
+        Cell cell = Cell.createCell(row, col, value, isFixed);
+
+        // Assert
         assertInstanceOf(FixedCell.class, cell);
         assertEquals(5, cell.getValue());
         assertFalse(cell.isEditable());
+    }
+
+    @Test
+    void shouldCreateEditableCell() {
+        // Arrange
+        int row = 0, col = 0, value = 0;
+        boolean isFixed = false;
+
+        // Act
+        Cell cell = Cell.createCell(row, col, value, isFixed);
+
+        // Assert
+        assertInstanceOf(EditableCell.class, cell);
+        assertEquals(0, cell.getValue());
+        assertTrue(cell.isEditable());
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidValue() {
+        // Arrange
+        int row = 0, col = 0, value = 10;
+        boolean isFixed = false;
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            Cell.createCell(row, col, value, isFixed);
+        });
     }
 
 }
